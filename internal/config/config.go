7ea99ff70fff0edf72d741febf239ca144fee647
kilanov <sam.kilanoff@gmail.com>
2025-01-21 07:59:43 +0500

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	Grpc        GrpcConfig    `yaml:"grpc"`
+}
+
+type GrpcConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	return &Config{}
+}
